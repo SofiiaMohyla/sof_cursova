c211318b5cb803896e1b2bcf549e379c6bb52735
Sofia Mohyla <sofiia.mohyla.kn.2021@lpnu.ua>
2023-05-15 15:47:42 +0300

    commit

--- Models/OrderViewModel.cs
+++ Models/OrderViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using sof_curs.Entity;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace sof_curs.Models
 {
@@ -21,6 +22,9 @@ namespace sof_curs.Models
 
         [Required(ErrorMessage = "Date is required")]
         [StringLength(100, MinimumLength = 4, ErrorMessage = "Please enter at least 4 chars")]
+        [DateGreaterThanOrEqualCurrentDate(ErrorMessage = "Please enter a date that is greater than or equal to the current date")]
+        [DateFormat(ErrorMessage = "Date format should be DD.MM.YYYY")]
+        [DateRange(ErrorMessage = "Incorrect input of date")]
         public string Date { get; set; }
 
         [Required(ErrorMessage = "Time is required")]
@@ -45,6 +49,61 @@ namespace sof_curs.Models
                 NumberOfPeople = NumberOfPeople,
                 Message = Message ?? "Empty"
             };
+        }
+    }
+
+    public class DateGreaterThanOrEqualCurrentDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && value is string dateString)
+            {
+                if (DateTime.TryParseExact(dateString, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+                {
+                    if (date >= DateTime.Today)
+                    {
+                        return ValidationResult.Success;
+                    }
+                }
+            }
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+
+    public class DateFormatAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string dateString)
+            {
+                string pattern = @"^\d{2}\.\d{2}\.\d{4}$";
+                if (Regex.IsMatch(dateString, pattern))
+                {
+                    return ValidationResult.Success;
+                }
+            }
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+
+    public class DateRangeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null && value is string dateString)
+            {
+                if (DateTime.TryParseExact(dateString, "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime date))
+                {
+                    if (date.Day >= 1 && date.Day <= 31 && date.Month >= 1 && date.Month <= 12 && date.Year >= 0)
+                    {
+                        return ValidationResult.Success;
+                    }
+                }
+            }
+
+            return new ValidationResult(ErrorMessage);
         }
     }
 }
